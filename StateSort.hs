fca4eac059bf5cef0c70c2ee30350150222b22dd
Ignat Insarov <kindaro@gmail.com>
2018-03-08 13:07:24 +0700

    Add naive myFunct to StateSort.

---
+++ StateSort.hs
@@ -0,0 +1,47 @@
+module StateSort where
+
+-- stackoverflow.com/q/49164810
+
+import Control.Monad.State
+
+myFunct' :: Ord a => [a] -> ([a], a)
+myFunct' xs = (reverse xs, minimum xs)
+
+-- λ myFunct' [1,2,3]
+-- ([3,2,1],1)
+
+stateExample1 :: State Integer Integer
+stateExample1 = do
+    x <- get
+    return $ x * 2  -- (* 2) is an example of a pure function.
+
+-- λ runState stateExample1 10
+-- (20,10)  -- The first is the return value, the second is the (unchanged) state.
+
+stateExample2 :: State Integer Integer
+stateExample2 = do
+    x <- get
+    put $ x * 2  -- You may think of it as though it were "x = x * 2" 
+                 -- in an imperative language.
+    return 0
+
+-- λ runState stateExample2 10
+-- (0,20)  -- Now we have changed the state, and return 0.
+
+myFunct :: Ord a => [a] -> State (Maybe a) [a]
+myFunct [ ] = return [ ]
+myFunct t = do
+        let s = reverse t
+        let a = minimum t
+        put (Just a)
+        return s
+
+-- λ runState (myFunct [1,2,3]) (Just (-100))
+-- ([3,2,1],Just 1)
+-- λ runState (myFunct []) (Just (-100))
+-- ([],Just (-100))
+
+-- λ myFunct' []
+-- ([],*** Exception: Prelude.minimum: empty list
+-- λ runState (myFunct []) Nothing
+-- ([],Nothing)
