238c088343781643b672e686f086e03b5708d1f1
Ignat Insarov <kindaro@gmail.com>
2018-07-24 22:12:37 +0700

    Remove spurious argument from inCollection.

--- default_collection_elem/Collection.hs
+++ default_collection_elem/Collection.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Collection where
 
 import Data.Tagged
 
+-- $setup
+--
+-- λ :set -XTypeApplications
+
 class Collected phantom
   where
-    type Element phantom
+    type Element phantom = r | r -> phantom
     type Element phantom = Tagged phantom Int
 
     type Collection phantom = r | r -> phantom
@@ -15,8 +20,7 @@ class Collected phantom
 
     collection :: Collection phantom
 
-    inCollection :: Int -> Collection phantom -> Maybe (Element phantom)
-    -- TODO: Remove the second argument, use the default collection automagically.
+    inCollection :: Int -> Maybe (Element phantom)
 
 data Primes
 
@@ -27,14 +31,14 @@ instance Collected Primes
 
     collection = Tagged [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 
-    inCollection element collection
-        | element `elem` unTagged collection = Just $ Tagged element
+    -- inCollection :: Int -> Maybe (Element Primes)
+    inCollection element
+        | element `elem` unTagged (collection @Primes) = Just $ Tagged element
         | otherwise = Nothing
 
 -- ^
--- λ inCollection 7 (collection :: Collection Primes)
+-- λ inCollection @Primes 7
 -- Just (Tagged 7)
--- λ inCollection 8 (collection :: Collection Primes)
+-- λ inCollection @Primes 8
 -- Nothing
-
 
