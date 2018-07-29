afc8c81cbb6cfc798ce6c30237adbb02137a3470
Ignat Insarov <kindaro@gmail.com>
2018-07-29 13:37:42 +0700

    Draft Yoneda isomorphism.

---
+++ solved/default-collection-elem/Yoneda.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE Rank2Types #-}
+
+-- | https://stackoverflow.com/q/51577631
+module Yoneda where
+
+newtype F a = F a deriving (Show, Functor)
+
+type G f a = forall a'. (a -> a') -> f a'
+
+fw x = x id
+
+bw :: Functor f => f a -> G f a
+bw x = \f -> fmap f x
