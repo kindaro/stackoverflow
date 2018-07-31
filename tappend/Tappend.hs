79ad39ca12fba4c313e2b3ae3af4e77ec1e6adc1
Ignat Insarov <kindaro@gmail.com>
2018-07-31 17:43:31 +0700

    Make Concat infix @ tappend.

--- tappend/Tappend.hs
+++ tappend/Tappend.hs
@@ -9,15 +9,15 @@
 
 module Tappend where
 
-type family Concat (ts :: [*]) (ts' :: [*])
+type family (ts :: [*]) ++ (ts' :: [*])
   where
-    Concat '[ ] ts = ts
-    Concat (t ': ts) ts' = t ': Concat ts ts'
+    '[ ] ++ ts = ts
+    (t ': ts) ++ ts' = t ': ts ++ ts'
 
 type family F (ts :: [*]) a
   where
     F '[ ] a = a
     F (t ': ts) a = t -> F ts a
 
-(...) :: F ts m -> F ts' m -> F (Concat ts ts') m
+(...) :: F ts m -> F ts' m -> F (ts ++ ts') m
 (...) f g = undefined
